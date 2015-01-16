8d0d2ff4573a06a234b9d6560b3d580f9d7122ba
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2015-01-16 14:33:17 +0300

    minor

--- src/moto/moto.h
+++ src/moto/moto.h
@@ -1,45 +1,51 @@
 /*
  * moto.h
  *
- *  Created on: 15 ÿíâ. 2015 ã.
+ *  Created on: 15 ï¿½ï¿½ï¿½. 2015 ï¿½.
  *      Author: andy
  */
 #ifndef MOTO_H_
 #define MOTO_H_
 
-#include <file.h>
+#include <fstream>
+#include <string>
+#include <queue>
 
 //Write an algorithm to implement git diff (Only text files diff)
 //git diff algorithm
 
-std::string git_diff(file* f1, file* f2) {
-  if (!f1 || !f2 )
-  throw(“File incorrect”);
+std::string git_diff(std::ifstream& f1, std::ifstream& f2) {
+  if (!f1.good() || !f2.good() )
+    throw("SOme file incorrect");
 
-  string s(“”);
-  stack st1;
-  stack st2;
+  std::string output;
+  std::queue q1;
+  std::queue q2;
+  std::string s1;
+  std::string s2;
   while( f1.good() && f2.good()) {
-    string s1 = f1.readline();
-    string s2 = f2.readline();
-    while ( s1.compare(s2) != 0 && st2.empty()) {
-      s.append(“ --- ” + s1+’\n’);
-      s1 = f1.readline();
+    std::getline(f1, s1);
+    std::getline(f2, s2);
+
+    while ( s1.compare(s2) != 0 && q2.empty()) {
+      s.append(" --- " + s1+"\n");
+//      s1 = f1.readline();
     }
     if (s1.compare(s2) != 0) {
-      st1.push(s1+’\n’);
-      st2.push(s2+’\n’);
+      q1.push(s1+"\n");
+      q2.push(s2+"\n");
     } else {
-      while (!st1.empty() ) {
-        s.append(“ --- ”st1.top());
-        st1.pop();
+      while (!q1.empty() ) {
+        s.append(" --- " + q1.front());
+        q1.pop();
       }
-      while (!st2.empty()) {
-        s.append(“ +++ ”st2.top());
-        st2.pop();
+      while (!q2.empty()) {
+        s.append(" +++ " + q2.front());
+        q2.pop();
       }
     }
   }
+  return s;
 
 }
 
@@ -47,28 +53,28 @@ Write an algorithm to implement git diff (Only text files diff)
 
 string git_diff(file* f1, file* f2) {
   if (!f1 || !f2 )
-  throw(“File incorrect”);
+  throw(ï¿½File incorrectï¿½);
 
-  string s(“”);
+  string s(ï¿½ï¿½);
   stack st1;
   stack st2;
   while( f1.good() && f2.good()) {
     string s1 = f1.readline();
     string s2 = f2.readline();
     while ( s1.compare(s2) != 0 && st2.empty()) {
-      s.append(“ --- ” + s1+’\n’);
+      s.append(ï¿½ --- ï¿½ + s1+ï¿½\nï¿½);
       s1 = f1.readline();
     }
     if (s1.compare(s2) != 0) {
-      st1.push(s1+’\n’);
-      st2.push(s2+’\n’);
+      st1.push(s1+ï¿½\nï¿½);
+      st2.push(s2+ï¿½\nï¿½);
     } else {
       while (!st1.empty() ) {
-        s.append(“ --- ”st1.top());
+        s.append(ï¿½ --- ï¿½st1.top());
         st1.pop();
       }
       while (!st2.empty()) {
-        s.append(“ +++ ”st2.top());
+        s.append(ï¿½ +++ ï¿½st2.top());
         st2.pop();
       }
     }
