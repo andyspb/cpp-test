78dae7a8af26b06fdd2db2ddb381f33b8ce666f4
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2016-07-25 16:41:08 +0300

    terramind added

--- src/teramind/terramind_1.h
+++ src/teramind/terramind_1.h
@@ -12,16 +12,21 @@ namespace terramind_1 {
 
 // If a struct has some members of type int and char, how do you compare two
 // instances of this struct to see if the members have the same value? *
-struct A {
-  int a;
-  char b;
+struct IntChar {
+  IntChar(int i, char c) {
+    int_ = i;
+    char_ = c;
+  }
+
+  int int_;
+  char char_;
 };
 
-int CompareA(const A& a1, const A& a2) {
+int CompareIntCharStruct(const IntChar& a1, const IntChar& a2) {
   long long l1 = 0;
   long long l2 = 0;
-  l1 = ((long long) a1.a << 32) + a1.b;
-  l2 = ((long long) a2.a << 32) + a2.b;
+  l1 = ((long long) a1.int_ << 32) + a1.char_;
+  l2 = ((long long) a2.int_ << 32) + a2.char_;
 
   if (l1 > l2)
     return 1;
@@ -33,6 +38,18 @@ int CompareA(const A& a1, const A& a2) {
 
 TEST_RESULT test() {
   __SCOPE_LOG__;
+
+  IntChar ic1(22, 33);
+  IntChar ic2(23, 34);
+  IntChar ic3(22, 33);
+
+  int comp1 = CompareIntCharStruct(ic1, ic2);
+  int comp2 = CompareIntCharStruct(ic2, ic3);
+  int comp3 = CompareIntCharStruct(ic1, ic3);
+
+  LOG(INFO) << "comp1=" << comp1;
+  LOG(INFO) << "comp2=" << comp2;
+  LOG(INFO) << "comp3=" << comp3;
 
   RETURN_OK();
 }
