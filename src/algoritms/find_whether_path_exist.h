29fcf1c8612b7b489bdc3c05ebc2d641111e1fbb
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-02-27 00:21:35 +0300

    added a new test

---
+++ src/algoritms/find_whether_path_exist.h
@@ -0,0 +1,25 @@
+/*
+ * find_whether_path_exist.h
+ *
+ *  Created on: Feb 26, 2017
+ *      Author: andy
+ */
+
+#ifndef SRC_ALGORITMS_FIND_WHETHER_PATH_EXIST_H_
+#define SRC_ALGORITMS_FIND_WHETHER_PATH_EXIST_H_
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+namespace find_whether_path_exist {
+
+
+
+
+}  // namespace find_whether_path_exist
+
+#endif /* SRC_ALGORITMS_FIND_WHETHER_PATH_EXIST_H_ */
